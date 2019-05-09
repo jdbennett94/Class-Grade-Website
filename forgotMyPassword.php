034620713317c13edafbc0772eec94234980d461
jdb94 <jdbennett1994@gmail.com>
2019-05-09 17:40:20 -0500

    Update forgotMyPassword.php

--- forgotMyPassword.php
+++ forgotMyPassword.php
@@ -19,7 +19,97 @@
 
 
 <div class = "separator">
-That username does not exist</div>
+<?php
+
+#Takes in html values
+$htmlUsername = $_POST['username'];
+$htmlEmail = $_POST['email'];
+$htmlColor = $_POST['color'];
+$htmlNewPassword = $_POST['password'];
+
+#Creates mysql connection
+$servername = "cs-database.cs.loyola.edu";
+$username = "jbennett";
+$password = "******";
+$dbName = "*****";
+$mysqli = new mysqli($servername, $username, $password, $dbName);
+if($mysqli -> connect_error) {
+        die("Database connection failed: " . $mysqli->connect_error);
+}
+
+#Verifies user data
+$query1 = "Select * from CS456Roster WHERE username='$htmlUsername' Limit 1";
+$result = $mysqli->query($query1);
+if(!$result) die($mysqli->error);
+$row = $result->fetch_assoc();
+if ($result->num_rows === 0) {
+        echo "That username does not exist";
+        goto a;
+}
+#Saves lastname
+$htmlLastname = $row['lastname'];
+
+#Verifies Security Questions
+if ($htmlEmail != $row['email']) {
+        echo "Your security information does not match our records";
+        goto a;
+}
+
+if ($htmlColor != $row['color']) {
+        echo "Your security information does not match our records";
+        goto a;
+}
+
+
+
+
+#### Deleting and inserting new values ####
+#Password hashing
+$cost = [
+    'cost' => 12,
+];
+$hashPswd =  password_hash($htmlNewPassword, PASSWORD_BCRYPT, $cost);
+
+
+#Deletes original values
+$queryDelete = "delete from CS456Roster where username='$htmlUsername'";
+if ($mysqli->query($queryDelete)=== true ) {
+        echo "Value successfully deleted";
+}
+else {
+        echo "Error: ", $queryDelete, "<br>", $conn->error;
+}
+
+#Inserts user values, you need the single apostrophies
+$query2 = "insert into CS456Roster(username, lastname, password, email, color) values('$htmlUsername', '$htmlLastname', '$hashPswd', '$htmlEmail', '$htmlColor')";
+
+if($mysqli->query($query2) === true) {
+
+        #Output success
+        echo "<div style ='text-align:center'>";
+        echo "Congrats!";
+        echo nl2br ("\n");
+        echo nl2br ("\n");
+        echo "Your password has been successfully changed!";
+        echo nl2br ("\n");
+        echo nl2br ("\n");
+        echo "You may now log into your account with your new password";
+        echo nl2br ("\n");
+        echo nl2br ("\n");
+        echo "Click <a href = 'home.html'>Here</a> to return to log-in page";
+        echo "</div>";
+
+}
+else {
+        echo "Error: ", $query2, "<br>", $conn->error;
+}
+
+
+a:
+$mysqli->close();
+
+?>
+</div>
 
 
 <div class = "footer">
@@ -41,4 +131,3 @@ Columbia, MD 21045<br>
 </div>
 
 </body></html>
-
