e806a76a6b324cab6e59964114b094ee7ebe1d14
jdb94 <jdbennett1994@gmail.com>
2019-05-09 17:42:38 -0500

    Update newStudent.php

--- newStudent.php
+++ newStudent.php
@@ -25,10 +25,82 @@
 
 
 <div class = "separator" style = "text-align: center">
-<SCRIPT LANGUAGE='JavaScript'>; 
+<?php
+$htmlUsername = $_POST['username'];
+$htmlPassword = $_POST['password'];
+$htmlLast = $_POST['lastname'];
+$htmlEmail = $_POST['email'];
+$htmlColor = $_POST['color']; 
+
+
+$servername = "cs-database.cs.loyola.edu";
+$username = "******";
+$password = "******";
+$dbName = "******";
+$mysqli = new mysqli($servername, $username, $password, $dbName);
+if($mysqli->connect_error) {
+        die("Database connection failed: " . $mysqli->connect_error);
+}
+
+#Checks to see if username exists
+$query = "select username from CS456Roster"; 
+$result = $mysqli->query($query);
+if(!$result) die($mysqli->error); 
+
+$rows = $result->num_rows; 
+$check = false; 
+
+#Iterates through searching for $username rows
+for ($j = 0; $j < $rows ; ++$j)
+{
+	$result -> data_seek($j);
+	$row = $result-> fetch_array(MYSQLI_ASSOC);
+	
+	if($row['username'] == $htmlUsername){	
+		#username exists
+
+		 echo ("<SCRIPT LANGUAGE='JavaScript'>; 
 			window.alert('That username already exists, please try again with a different one'); 
 			window.location.href='http://jbennett.cs.loyola.edu/projects/projectFinal/html/newStudent.html'; 
-			</SCRIPT>Error: insert into CS456Roster(username, lastname, password, email, color) values('', '', '$2y$12$U6ASDPNWydF5..pbkGHinOm/guO4qISl.oYWIXKc5S8TOyFtOIk4a', '', '')<br>Duplicate entry '' for key 'PRIMARY'Error: insert into CS456Grades(username,lastname,a1,a2,a3,a4,a5) values('', '', '', '', '', '', '')<br>Duplicate entry '' for key 'PRIMARY'<br> 
+			</SCRIPT>");    
+	}
+}
+
+#Password hashing
+$cost = [
+    'cost' => 12,
+];
+$hashPswd =  password_hash($htmlPassword, PASSWORD_BCRYPT, $cost);
+
+#Inserts user values, you need the single apostrophies
+$query2 = "insert into CS456Roster(username, lastname, password, email, color) values('$htmlUsername', '$htmlLast', '$hashPswd', '$htmlEmail', '$htmlColor')"; 
+if($mysqli->query($query2) === true) {
+	echo "Congratulations, you have been added to the course properly.";
+	echo nl2br ("\n");
+	echo nl2br ("\n");
+	echo "You may now log in with the credentials you have provided.";
+	echo nl2br ("\n");
+}
+else {
+	echo "Error: ", $query2, "<br>", $mysqli->error; 
+}
+
+
+#### Create grade roster ####
+$queryDG = "insert into CS456Grades(username,lastname,a1,a2,a3,a4,a5) values('$htmlUsername', '$htmlLast', '', '', '', '', '')";
+
+if ($mysqli->query($queryDG) === true) {
+	echo "Good";	
+}
+else {
+	echo "Error: ", $queryDG, "<br>", $mysqli->error;
+}
+
+
+#closes out of database connections
+$mysqli->close(); 
+
+?><br> 
 <div style = "text-align:center">Click <a href = "home.html">Here</a> To Go To Log-In Page</div> 
 </div>
 
@@ -51,4 +123,6 @@ Columbia, MD 21045<br>
 410-617-7600</p>
 </div>
 
-</body></html> 
+</body></html>
+
+
