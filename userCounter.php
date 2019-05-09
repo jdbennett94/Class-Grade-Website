6805ca02a6a5c33e4b80d7052150b07639ef2de2
jdb94 <jdbennett1994@gmail.com>
2019-05-09 17:45:52 -0500

    Update userCounter.php

--- userCounter.php
+++ userCounter.php
@@ -1,2 +1,23 @@
 <!-- This is the php script to keep track of user # -->
-3801
+<?php 
+
+#Checks if file exists, increments if yes, creates & begins if no
+if(file_exists('userCount.txt')) {	
+	$txtFile = fopen('userCount.txt',r);  
+	$txtValue = fread($txtFile, filesize('userCount.txt'));
+	
+	echo ($txtValue+1);
+		
+	fclose($txtFile);
+
+	$txtFile = fopen('userCount.txt',w);
+	fwrite($txtFile, $txtValue+1); 
+}
+else {
+	$txtFile = fopen('userCount.txt', w);
+	fwrite($txtFile,1);
+	echo '1'; 
+	fclose($txtFile);
+}
+
+?>
